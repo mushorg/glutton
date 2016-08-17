c5d98beb6f1eff1f639c684eff6f23d5bcec23b8
Lukas Rist <glaslos@gmail.com>
2016-08-17 23:30:37 +0200

    Update glutton_test.go

--- glutton_test.go
+++ glutton_test.go
@@ -3,7 +3,7 @@ package glutton
 import "testing"
 
 func TestPort2Protocol(t *testing.T) {
-	prot := getProtocol(80, "tcp")
+	prot := GetProtocol(80, "tcp")
 	if prot.Name != "http" {
 		t.Fatalf("Got %s instead of http", prot.Name)
 	}
