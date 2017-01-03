ffd50802c63e83cf8f3fab874f184bc73d8aa7ac
glaslos <lukas.rist@bluecoat.com>
2017-01-03 16:42:03 +0100

    fixed reading body

--- http.go
+++ http.go
@@ -2,6 +2,7 @@ package glutton
 
 import (
 	"bufio"
+	"bytes"
 	"log"
 	"net"
 	"net/http"
@@ -17,9 +18,14 @@ func handleHTTP(conn net.Conn) error {
 	log.Printf("%+v", req)
 	if req.ContentLength > 0 {
 		defer req.Body.Close()
-		buf := new([]byte)
-		req.Body.Read(*buf)
-		log.Println(string(*buf))
+		buf := bytes.NewBuffer(make([]byte, 0, req.ContentLength))
+		_, err = buf.ReadFrom(req.Body)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		body := buf.Bytes()
+		log.Println(string(body))
 	}
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	return nil
