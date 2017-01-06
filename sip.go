34c5b32c51a7996dae99977bbd4b0959ef675299
glaslos <lukas.rist@bluecoat.com>
2017-01-06 16:01:41 +0100

    added OPTIONS handling for SIP

--- sip.go
+++ sip.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"net"
 
-	"github.com/1lann/go-sip/server"
-	"github.com/1lann/go-sip/sipnet"
+	"github.com/glaslos/go-sip/server"
+	"github.com/glaslos/go-sip/sipnet"
 )
 
 func handleSIP(netConn net.Conn) {
-	sipConn := &sipnet.Conn{}
+	sipConn := &sipnet.Conn{
+		Conn: netConn,
+	}
 	buf := []byte{}
 	rd := bytes.NewReader(buf)
 	req, err := sipnet.ReadRequest(rd)
@@ -25,5 +27,17 @@ func handleSIP(netConn net.Conn) {
 	case sipnet.MethodInvite:
 		log.Println("handling SIP invite")
 		server.HandleInvite(req, sipConn)
+	case sipnet.MethodOptions:
+		log.Println("handling SIP options")
+		resp := sipnet.NewResponse()
+		resp.StatusCode = sipnet.StatusOK
+		resp.Header.Set("Allow", "INVITE, ACK, CANCEL, OPTIONS, BYE")
+		resp.Header.Set("Accept", "application/sdp")
+		resp.Header.Set("Accept-Encoding", "gzip")
+		resp.Header.Set("Accept-Language", "en")
+		resp.Header.Set("Content-Type", "application/sdp")
+		resp.Body = req.Body
+		resp.WriteTo(sipConn, req)
+		break
 	}
 }
