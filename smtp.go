6a7522da6fc5986329b4082234e64235ec0f8bf6
Mehul Ahuja <blackdragon0909@gmail.com>
2017-04-01 00:45:32 +0530

    smtp responses fixed and random wait added

--- smtp.go
+++ smtp.go
@@ -2,7 +2,11 @@ package glutton
 
 import (
 	"bufio"
+	"math/rand"
 	"net"
+	"regexp"
+	"strings"
+	"time"
 )
 
 // Client is a connection container
@@ -24,6 +28,29 @@ func (c *Client) r(g *Glutton) string {
 	return reply
 }
 
+func rwait() {
+	// makes the process sleep for random time
+	rand.Seed(time.Now().Unix())
+	// between 0.5 - 1.5 seconds
+	rtime := rand.Intn(1500) + 500
+	duration := time.Duration(rtime) * time.Millisecond
+	time.Sleep(duration)
+}
+func validateMail(query string) bool {
+	email := regexp.MustCompile("^MAIL FROM:<.+@.+>$") // naive regex
+	if email.MatchString(query) {
+		return true
+	}
+	return false
+}
+func validateRCPT(query string) bool {
+	rcpt := regexp.MustCompile("^RCPT TO:<.+@.+>$")
+	if rcpt.MatchString(query) {
+		return true
+	}
+	return false
+}
+
 // HandleSMTP takes a net.Conn and does basic SMTP communication
 func (g *Glutton) HandleSMTP(conn net.Conn) {
 	defer conn.Close()
@@ -32,14 +59,31 @@ func (g *Glutton) HandleSMTP(conn net.Conn) {
 		bufin:  bufio.NewReader(conn),
 		bufout: bufio.NewWriter(conn),
 	}
-	client.w("220 Welcome")
-	g.logger.Infof("[smpt    ] Payload 1: %q", client.r(g))
-	client.w("250 Is it me?")
-	g.logger.Infof("[smpt    ] Payload 2: %q", client.r(g))
-	client.w("250 Sender")
-	g.logger.Infof("[smpt    ] Payload 3: %q", client.r(g))
-	client.w("250 Recipient")
-	g.logger.Infof("[smpt    ] Payload 4: %q", client.r(g))
-	client.w("354 Ok Send data ending with <CRLF>.<CRLF>")
-	g.logger.Infof("[smpt    ] Payload 5: %q", client.r(g))
+	rwait()
+	client.w("220 Welcome!")
+	for {
+		query := strings.Trim(client.r(g), "\r\n")
+		g.logger.Infof("[smtp    ] Payload : %q", query)
+		if strings.HasPrefix(query, "HELO ") {
+			rwait()
+			client.w("250 Hello! Pleased to meet you.")
+		} else if validateMail(query) {
+			rwait()
+			client.w("250 OK")
+		} else if validateRCPT(query) {
+			rwait()
+			client.w("250 OK")
+		} else if strings.Compare(query, "DATA") == 0 {
+			client.w("354 End data with <CRLF>.<CRLF>")
+			for strings.Compare(client.r(g), ".\r\n") != 0 {
+			}
+			rwait()
+			client.w("250 OK")
+		} else if strings.Compare(query, "QUIT") == 0 {
+			client.w("Bye")
+			break
+		} else {
+			client.w("Recheck the command you entered.")
+		}
+	}
 }
