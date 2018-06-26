ca261aa3bb41dfd4a82553b4ebed94ca5ffcb97f
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2018-06-26 17:48:03 +0500

    Added full support for TELNET proxy

--- protocols.go
+++ protocols.go
@@ -50,7 +50,9 @@ func (g *Glutton) mapProtocolHandlers() {
 	g.protocolHandlers["proxy_ssh"] = func(ctx context.Context, conn net.Conn) (err error) {
 		return g.sshProxy.handle(ctx, conn)
 	}
-
+	g.protocolHandlers["proxy_telnet"] = func(ctx context.Context, conn net.Conn) (err error) {
+		return g.telnetProxy.handle(ctx, conn)
+	}
 	g.protocolHandlers["default"] = func(ctx context.Context, conn net.Conn) (err error) {
 		// TODO: remove 'context.TODO()' when handler code start using context.
 		ctx = context.TODO()
