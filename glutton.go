66a251004778ccac6bb77ae4c057092bd3b6497c
Lukas Rist <lr@cortilabs.com>
2018-11-23 12:30:03 +0100

    test fix

--- glutton.go
+++ glutton.go
@@ -35,18 +35,17 @@ type protocolHandlerFunc func(ctx context.Context, conn net.Conn) error
 
 // InitConfig initializes the configuration
 func InitConfig(logger *zap.Logger) (err error) {
-	v := viper.New()
 	// Loading config file
-	v.SetConfigName("conf")
-	v.AddConfigPath(viper.GetString("confpath"))
-	if err = v.ReadInConfig(); err != nil {
+	viper.SetConfigName("conf")
+	viper.AddConfigPath(viper.GetString("confpath"))
+	if err = viper.ReadInConfig(); err != nil {
 		return
 	}
 	// If no config is found, use the defaults
-	v.SetDefault("glutton_server", 5000)
-	v.SetDefault("rules_path", "rules/rules.yaml")
-	v.SetDefault("gollumAddress", "http://gollum:gollum@localhost:9000")
-	v.SetDefault("enableGollum", false)
+	viper.SetDefault("glutton_server", 5000)
+	viper.SetDefault("rules_path", "rules/rules.yaml")
+	viper.SetDefault("gollumAddress", "http://gollum:gollum@localhost:9000")
+	viper.SetDefault("enableGollum", false)
 
 	logger.Debug("configuration loaded successfully", zap.String("reporter", "glutton"))
 	return
