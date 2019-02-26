8a43aa1b00c504a9e0150cc90a4e61e55b54c311
Sladyn <gunnerforlife00@gmail.com>
2019-02-26 19:17:57 +0530

    smtp_test.go: Add test for email validity (#123)
    Added tests for email validity check.

---
+++ smtp_test.go
@@ -0,0 +1,27 @@
+package glutton
+
+import (
+	"testing"
+)
+
+func TestValidateMail(t *testing.T) {
+
+	if validateMail("MAIL FROM:<example@example.com>") != true {
+		t.Fatal("Validate email regex failed")
+	}
+
+	if validateMail("MAIL FROM:<example.com>") == true {
+		t.Fatal("Validate email regex failed")
+	}
+}
+
+func TestValidateRCPT(t *testing.T){
+
+	if validateRCPT("RCPT TO:<example@example.com>") != true {
+		t.Fatal(validateRCPT("Validate rcpt regex failed"))
+	}
+
+	if validateRCPT("RCPT TO:<example.com>") == true {
+		t.Fatal(validateRCPT("Validate rcpt regex failed"))
+	}
+}
