819a448318392679f756206c99398d094380900d
Sladyn <gunnerforlife00@gmail.com>
2019-03-02 01:53:58 +0530

    system_test.go:Add unit tests (#125)

---
+++ system_test.go
@@ -0,0 +1,19 @@
+package glutton
+
+import (
+	"testing"
+)
+
+func TestCountOpenFiles(t *testing.T) {
+	openFiles := countOpenFiles()
+	if openFiles <= 0 {
+		t.Fatalf("countOpenFiles returned %d, expected > 0", openFiles)
+	}
+}
+
+func TestCountRunningRoutines(t *testing.T) {
+	runningRoutines := countRunningRoutines()
+	if runningRoutines <= 0 {
+		t.Fatalf("runningRoutines returned %d, expected > 0", runningRoutines)
+	}
+}
