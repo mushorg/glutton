24530362d47cdb26624f057130b7063a27fccd7c
Lukas Rist <glaslos@gmail.com>
2023-07-18 00:03:31 +0200

    add and fix test

--- connection/conntable_test.go
+++ connection/conntable_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/mushorg/glutton/rules"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,11 +22,11 @@ func TestNewConnKeyFromNetConn(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, ln)
 	defer ln.Close()
-	con, err := net.Dial(ln.Addr().Network(), ln.Addr().String())
+	conn, err := net.Dial(ln.Addr().Network(), ln.Addr().String())
 	require.NoError(t, err)
-	require.NotNil(t, con)
-	defer con.Close()
-	ck, err := NewConnKeyFromNetConn(con)
+	require.NotNil(t, conn)
+	defer conn.Close()
+	ck, err := NewConnKeyFromNetConn(conn)
 	require.NoError(t, err)
 	require.Equal(t, testck, ck)
 }
@@ -38,17 +39,34 @@ func TestNewConnTable(t *testing.T) {
 func TestRegister(t *testing.T) {
 	table := New()
 	targetPort := 4321
-	err := table.Register("127.0.0.1", "1234", uint16(targetPort))
+	err := table.Register("127.0.0.1", "1234", uint16(targetPort), &rules.Rule{})
 	require.NoError(t, err)
 	m := table.Get(testck)
 	require.NotNil(t, m)
 	require.Equal(t, targetPort, int(m.TargetPort))
 }
 
+func TestRegisterConn(t *testing.T) {
+	ln, err := net.Listen("tcp4", "127.0.0.1:1234")
+	require.NoError(t, err)
+	require.NotNil(t, ln)
+	defer ln.Close()
+	conn, err := net.Dial(ln.Addr().Network(), ln.Addr().String())
+	require.NoError(t, err)
+	require.NotNil(t, conn)
+	defer conn.Close()
+	table := New()
+	err = table.RegisterConn(conn, &rules.Rule{Target: "default"})
+	require.NoError(t, err)
+	m := table.Get(testck)
+	require.NotNil(t, m)
+	require.Equal(t, "default", m.Rule.Target)
+}
+
 func TestFlushOlderThan(t *testing.T) {
 	table := New()
 	targetPort := 4321
-	err := table.Register("127.0.0.1", "1234", uint16(targetPort))
+	err := table.Register("127.0.0.1", "1234", uint16(targetPort), &rules.Rule{})
 	require.NoError(t, err)
 	table.FlushOlderThan(time.Duration(0))
 	m := table.Get(testck)
